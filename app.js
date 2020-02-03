08165cd616b7cb23f92ff7bc6feea44d4d0fb035
aumezawa <letzt.apostel@gmail.com>
2020-02-03 21:16:03 +0900

    v1.6.1 - Refactoring for HTTPS

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ import project from "./package.json"
 
 const app = express()
 
+app.set("http_port", process.env.HTTP_PORT || project.http_port || 80)
+app.set("https_port", process.env.HTTPS_PORT || project.https_port || 443)
+app.set("http_enable", project.http_enable)
+
 app.use(logger("dev"));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
